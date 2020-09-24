9d535b1ab7106e2383b30f689a75c7744ebd036c
rafael-ravena <rafael.ravena@gmail.com>
2020-09-24 10:19:04 -0300

    setembro 2020

---
+++ Scripts/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    if (docWidth > mediaBreakpoint) {
+        ParallaxSections();
+        ParallaxThem()
+    }
+    $(document).on("mousemove", function(event) {
+        mousePos.X = event.pageX;
+        mousePos.Y = event.pageY;
+        if (docWidth > mediaBreakpoint) {
+            ParallaxSections();
+        }
+    }).on("scroll", function() {
+        if (docWidth > mediaBreakpoint) {
+            ParallaxThem();
+        }
+    });
+});
+var mediaBreakpoint = 799.99;
+var mousePos = { X: 0, Y: 0 };
+var docWidth = $(window).width();
+var docHeight = $(window).height();
+
+function ParallaxThem() {
+    ParallaxContent("#Objetivo");
+    ParallaxContent("#Qualificacoes");
+    ParallaxContent("#Formacao");
+    ParallaxContent("#Cursos");
+    ParallaxContent("#Idiomas");
+}
+
+function ParallaxContent(element) {
+    var docOffsetY = $(document).scrollTop();
+    var elementOffsetY = $(element).offset().top;
+    var sectionFactor = (docHeight / 20) + (docOffsetY - elementOffsetY) / 5
+    $(element).children(".holder").css("margin-top", sectionFactor + "px");
+}
+
+function ParallaxSections() {
+    $("section").css("background-position", GetMouseX() + GetMouseY());
+}
+
+function GetMouseX() {
+    var factor = ((docWidth - mousePos.X) / 2);
+    return (-docWidth / 50) + (factor / 30) + "px ";
+}
+
+function GetMouseY() {
+    var factor = ((docHeight - mousePos.Y) / 2);
+    return (-docHeight / 50) + (factor / 30) + "px";
+}
