120e4a8baa9d1b578811c2bbd8929aa2f498e078
rafael-ravena <rafael.ravena@gmail.com>
2024-08-08 23:03:54 -0300

    b4 react migration

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,18 +1,9 @@
-$(document).ready(function() {
-    if (docWidth > mediaBreakpoint) {
-        ParallaxSections();
-        ParallaxThem()
-    }
+$(document).ready(function () {
     $(document).on("mousemove", function (event) {
         mousePos.X = event.pageX;
-        mousePos.Y = event.pageY;
-        if (docWidth > mediaBreakpoint) {
-            ParallaxSections();
-        }
+        ParallaxSections();
     }).on("scroll", function () {
-        if (docWidth > mediaBreakpoint) {
-            ParallaxThem();
-        }
+        ParallaxSections();
     }).on("click", "#Lang-selector > .inner-wrapper > a", function (e) {
         e.preventDefault();
         e.stopPropagation();
@@ -23,38 +14,46 @@ $(document).ready(function() {
         }
         window.location.href = nextLocation;
     });
+    SetBackground();
+    ShowAge();
 });
-var mediaBreakpoint = 799.99;
 var mousePos = { X: 0, Y: 0 };
 var docWidth = $(window).width();
 var docHeight = $(window).height();
-
-function ParallaxThem() {
-    ParallaxContent("#Objetivo");
-    ParallaxContent("#Qualificacoes");
-    ParallaxContent("#Formacao");
-    ParallaxContent("#Cursos");
-    ParallaxContent("#Idiomas");
-}
-
-function ParallaxContent(element) {
-    var docOffsetY = $(document).scrollTop();
-    var elementOffsetY = $(element).offset().top;
-    var sectionFactor = (docHeight / 20) + (docOffsetY - elementOffsetY) / 5
-    $(element).children(".holder").css("margin-top", sectionFactor + "px");
-}
+var bg = ["/Content/images/goals.jpg",
+    "/Content/images/study.jpg",
+    "/Content/images/xperience.jpg",
+    "/Content/images/skills.jpg"];
 
 function ParallaxSections() {
-    $("section").css("background-position", GetMouseX() + GetMouseY());
+    $("body").css("background-position", GetMouseX() + GetMouseY());
 }
 
 function GetMouseX() {
-    var factor = ((docWidth - mousePos.X) / 2);
-    return (-docWidth / 50) + (factor / 30) + "px ";
+    // var factor = ((docWidth - mousePos.X));
+    // return (-docWidth / 4) + (factor / 30) + "px ";
+    return "center ";
 }
 
 function GetMouseY() {
-    var factor = ((docHeight - mousePos.Y) / 2);
-    return (-docHeight / 50) + (factor / 30) + "px";
+    var docOffsetY = $(document).scrollTop();
+    return (-docHeight / 50) + (-docOffsetY / 15) + "px";
 }
 
+function SetBackground() {
+    const bgCopy = bg.slice();
+    bgCopy.push(...bgCopy);
+    bgCopy.push(...bgCopy);
+    var bgIndex = Math.floor(Math.random() * bgCopy.length);
+    $("body").css("background-image", "url('" + bgCopy[bgIndex] + "')");
+    ParallaxSections();
+}
+
+function ShowAge() {
+    var birthDate = new Date(1981, 6, 9);
+    var ageDifMs = Date.now() - birthDate.getTime();
+    var ageDate = new Date(ageDifMs);
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    console.log(age);
+    $("[data-behavior~='display-age']").text(age);
+}
